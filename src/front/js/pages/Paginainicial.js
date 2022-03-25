bae571d39e1633802b8a788bde9421586b04b8e2
YasZamora <yzamo001@gmail.com>
2022-03-25 23:44:43 +0000

    cambio página inicial

--- src/front/js/pages/Paginainicial.js
+++ src/front/js/pages/Paginainicial.js
@@ -3,14 +3,14 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import Login from "./login.js";
-import {Row, Card, Col} from "react-bootstrap";
+import { Row, Card, Col } from "react-bootstrap";
 
 const Paginainicial = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<>
-		{/* // <div className="text-center mt-5">
+  return (
+    <>
+      {/* // <div className="text-center mt-5">
 		// 	<h1>Hello Rigo!!</h1>
 		// 	<p>
 		// 		<img src={rigoImageUrl} />
@@ -27,25 +27,28 @@ const Paginainicial = () => {
 		// </div>
 		// <Login /> */}
 
-        <Row xs={1} md={2} className="g-4">
-  {Array.from({ length: 4 }).map((_, idx) => (
-    <Col>
-      <Card>
-        <Card.Img variant="top" src="https://picsum.photos/id/233/100/100" />
-        <Card.Body>
-          <Card.Title>Curso SayanDevelopers</Card.Title>
-          <Card.Text>
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
-  ))}
-</Row>
-<Login />
-
-		</>
-	);
+      <Row xs={1} md={2} className="g-4">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col>
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://picsum.photos/id/233/50/50"
+              />
+              <Card.Body>
+                <Card.Title>Curso SayanDevelopers</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+      <Login />
+    </>
+  );
 };
 export default Paginainicial;
