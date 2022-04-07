006bed3f614b57deab6fb13bf0422a130828fdf5
MatiasGonzalezL <92063536+MatiasGonzalezL@users.noreply.github.com>
2022-04-06 21:33:02 -0400

    Delete Footer.js

--- Footer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import '../../styles/footer.css';
-
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		Made by SayanDevelopers, 2022
-		
-	</footer>
-);
